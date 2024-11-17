bd68d8d8656c7a908b1e35878247118eb27c05a6
Ryan Joel Castro <ryanjoelimba@gmail.com>
2024-11-17 22:24:49 +0800

    added prisma config for deployment

--- components/SearchArrayProvider.jsx
+++ components/SearchArrayProvider.jsx
@@ -5,65 +5,65 @@ import axios from "axios";
 export const SearchArrayDataProvider = createContext(null);
 
 const SearchArrayProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [searchData, setSearchData] = useState("");
-  const [searchDataArchives, setSearchDataArchives] = useState("");
+	const [users, setUsers] = useState([]);
+	const [searchData, setSearchData] = useState("");
+	const [searchDataArchives, setSearchDataArchives] = useState("");
 
-  useEffect(() => {
-    const getUsers = async () => {
-      try {
-        const usersData = await axios.get("/api/user");
-        const data = usersData.data;
-        setUsers(data);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    };
+	useEffect(() => {
+		const getUsers = async () => {
+			try {
+				const usersData = await axios.get(`/api/user?timestamp=${Date.now()}`);
+				const data = usersData.data;
+				setUsers(data);
+			} catch (error) {
+				console.error("Error fetching users:", error);
+			}
+		};
 
-    getUsers();
-  }, []);
-  // ken remove user in dependency
-  const notAdminUsers = users.filter(
-    (val) => val.userType !== "admin" && val.certificatedApproved === false
-  );
-  const graudatedUsers = users.filter(
-    (val) => val.certificatedApproved === true
-  );
+		getUsers();
+	}, []);
+	// ken remove user in dependency
+	const notAdminUsers = users.filter(
+		(val) => val.userType !== "admin" && val.certificatedApproved === false
+	);
+	const graudatedUsers = users.filter(
+		(val) => val.certificatedApproved === true
+	);
 
-  const finalUsers = notAdminUsers.filter((val) => {
-    const lowercaseSearch = searchData.toLowerCase();
-    // Check if the name or any other data fields contain the search query
-    return Object.values(val).some(
-      (field) =>
-        typeof field === "string" &&
-        field.toLowerCase().includes(lowercaseSearch)
-    );
-  });
+	const finalUsers = notAdminUsers.filter((val) => {
+		const lowercaseSearch = searchData.toLowerCase();
+		// Check if the name or any other data fields contain the search query
+		return Object.values(val).some(
+			(field) =>
+				typeof field === "string" &&
+				field.toLowerCase().includes(lowercaseSearch)
+		);
+	});
 
-  const archivedUsers = graudatedUsers.filter((val) => {
-    const lowercaseSearch = searchDataArchives.toLowerCase();
-    // Check if the name or any other data fields contain the search query
-    return Object.values(val).some(
-      (field) =>
-        typeof field === "string" &&
-        field.toLowerCase().includes(lowercaseSearch)
-    );
-  });
+	const archivedUsers = graudatedUsers.filter((val) => {
+		const lowercaseSearch = searchDataArchives.toLowerCase();
+		// Check if the name or any other data fields contain the search query
+		return Object.values(val).some(
+			(field) =>
+				typeof field === "string" &&
+				field.toLowerCase().includes(lowercaseSearch)
+		);
+	});
 
-  return (
-    <SearchArrayDataProvider.Provider
-      value={{
-        finalUsers,
-        searchData,
-        setSearchData,
-        archivedUsers,
-        searchDataArchives,
-        setSearchDataArchives,
-      }}
-    >
-      {children}
-    </SearchArrayDataProvider.Provider>
-  );
+	return (
+		<SearchArrayDataProvider.Provider
+			value={{
+				finalUsers,
+				searchData,
+				setSearchData,
+				archivedUsers,
+				searchDataArchives,
+				setSearchDataArchives,
+			}}
+		>
+			{children}
+		</SearchArrayDataProvider.Provider>
+	);
 };
 
 export default SearchArrayProvider;
