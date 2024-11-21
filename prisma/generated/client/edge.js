205b9ca746d142fe6c50a34499bfc5a4de0a6bc8
Ryan Joel Castro <ryanjoelimba@gmail.com>
2024-11-21 23:09:09 +0800

    changes were made

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -208,11 +208,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-<<<<<<< HEAD
       "value": "C:\\Users\\ryanj\\Desktop\\Programming\\next js\\red_cross_collaboration\\red-cross-admin-Web\\prisma\\generated\\client",
-=======
-      "value": "C:\\Users\\amand\\Desktop\\capstoneSytem\\Admin-Web\\prisma\\generated\\client",
->>>>>>> 8baf3134c58abf8aac5b1c38e49ec150de9209a8
       "fromEnvVar": null
     },
     "config": {
@@ -226,19 +222,11 @@ const config = {
       }
     ],
     "previewFeatures": [],
-<<<<<<< HEAD
     "sourceFilePath": "C:\\Users\\ryanj\\Desktop\\Programming\\next js\\red_cross_collaboration\\red-cross-admin-Web\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
-=======
-    "sourceFilePath": "C:\\Users\\amand\\Desktop\\capstoneSytem\\Admin-Web\\prisma\\schema.prisma",
-    "isCustomOutput": true
-  },
-  "relativeEnvPaths": {
     "rootEnvPath": null,
->>>>>>> 8baf3134c58abf8aac5b1c38e49ec150de9209a8
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
@@ -248,10 +236,6 @@ const config = {
     "db"
   ],
   "activeProvider": "mongodb",
-<<<<<<< HEAD
-  "postinstall": true,
-=======
->>>>>>> 8baf3134c58abf8aac5b1c38e49ec150de9209a8
   "inlineDatasources": {
     "db": {
       "url": {
