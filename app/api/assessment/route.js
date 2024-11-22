7337f04713ab6f69b4b1d849e25a341b2b325368
ryanjoel24 <ryanjoelimba@gmail.com>
2024-11-22 10:57:44 +0800

    fixing again

--- app/api/assessment/route.js
+++ app/api/assessment/route.js
@@ -1,29 +1,26 @@
-import prisma from '@/libs/prismaDB'
-import { NextResponse } from 'next/server'
+import prisma from "@/libs/prismaDB";
+import { NextResponse } from "next/server";
 
-export async function POST(req){
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { firstAid, cpr, pathogen, preparedness, teamwork, userId } = body;
 
-    try {
-        const body = await req.json();
-        const { firstAid, cpr, pathogen, preparedness, teamwork, userId } = body;
+    // Insert new UserAssessment into the database
+    const newAssessment = await prisma.userAssessment.create({
+      data: {
+        firsAid: firstAid,
+        cpr: cpr,
+        pathogen: pathogen,
+        preparedness: preparedness,
+        teamwork: teamwork,
+        userId: userId,
+      },
+    });
 
-        // Insert new UserAssessment into the database
-        const newAssessment = await prisma.userAssessment.create({
-            data: {
-                firsAid: firstAid,
-                cpr: cpr,
-                pathogen: pathogen,
-                preparedness: preparedness,
-                teamwork: teamwork,
-                userId: userId,
-            },
-        });
-
-        return NextResponse.json(newAssessment);
-    } catch (error) {
-        console.error('Error creating UserAssessment:', error);
-        return NextResponse.error(new Error('Failed to create UserAssessment'));
-    }
-
-    
+    return NextResponse.json(newAssessment);
+  } catch (error) {
+    console.error("Error creating UserAssessment:", error);
+    return NextResponse.error(new Error("Failed to create UserAssessment"));
+  }
 }
