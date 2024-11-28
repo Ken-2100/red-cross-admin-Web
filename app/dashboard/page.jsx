9389c45680433812c5116890433582356a1f7b56
ryanjoel24 <ryanjoelimba@gmail.com>
2024-11-28 11:46:31 +0800

    configuring the data

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -10,70 +10,56 @@ import Link from "next/link";
 import { RiUploadCloud2Fill } from "react-icons/ri";
 import { SideNavigationProvider } from "@/components/SideNavigationProvider";
 import Analytics from "@/components/Analytics";
-import { toast } from "react-toastify";
 import InstructorsPage from "@/components/InstructorsPage";
+import SearchArrayProvider from "@/components/SearchArrayProvider";
 import UploadCerts from "@/components/UploadCerts";
 const Dashboard = () => {
-	const navigationData = useContext(SideNavigationProvider);
+  const navigationData = useContext(SideNavigationProvider);
+  const usersFlag = useContext(SearchArrayProvider);
 
-	const [searchData, setSearchData] = useState("");
-	const [unlockCertificate, setUnlockCertificate] = useState(false);
+  const [searchData, setSearchData] = useState("");
+  const [unlockCertificate, setUnlockCertificate] = useState(false);
+  const [flag, setFlag] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [update, setUpdate] = useState(false);
+  const [updateId, setUpdateId] = useState("");
 
-	const [loading, setLoading] = useState(false);
-	const [update, setUpdate] = useState(false);
-	const [updateId, setUpdateId] = useState("");
-	const [users, setUsers] = useState([]);
+  const handleDelete = async (id) => {
+    const data = await axios.delete(`/api/user/${id}`);
+  };
 
-	const handleDelete = async (id) => {
-		await axios
-			.delete(`/api/user/${id}`)
-			.then(() => {
-				toast("Trainee Has Been Deleted", {
-					position: "top-center",
-					autoClose: 5000,
-					hideProgressBar: false,
-					closeOnClick: true,
-					pauseOnHover: true,
-					draggable: true,
-					progress: undefined,
-					theme: "light",
-				});
-			})
-			.catch((e) => alert(e.message));
-	};
+  const handleUnlockCertificate = async (id) => {
+    await axios
+      .patch(`/api/certificate/${id}`, { formattedDate: formattedDate })
+      .then(() => {
+        alert("user certificate has been unlock!");
+      })
+      .catch((e) => alert(e.message));
+  };
 
-	const handleUnlockCertificate = async (id) => {
-		await axios
-			.patch(`/api/certificate/${id}`, { formattedDate: formattedDate })
-			.then(() => {
-				alert("user certificate has been unlock!");
-			})
-			.catch((e) => alert(e.message));
-	};
-
-	return (
-		<div className="flex justify-center items-center w-full bg-white h-full">
-			{navigationData.navigation === "form" && (
-				<RegisterForm
-					update={update}
-					setUpdate={setUpdate}
-					navigationDataChange={navigationData.setNavigation}
-					loading={loading}
-					setLoading={setLoading}
-				/>
-			)}
-			{navigationData.navigation === "list" && (
-				<DataTable
-					handleUnlockCertificate={handleUnlockCertificate}
-					handleDelete={handleDelete}
-				/>
-			)}
-			{navigationData.navigation === "analytics" && <Analytics />}
-			{navigationData.navigation === "archive" && <Archives />}
-			{navigationData.navigation === "instructors" && <InstructorsPage />}
-			{navigationData.navigation === "uploadCerts" && <UploadCerts />}
-		</div>
-	);
+  return (
+    <div className="flex justify-center items-center w-full bg-white h-full">
+      {navigationData.navigation === "form" && (
+        <RegisterForm
+          update={update}
+          setUpdate={setUpdate}
+          navigationDataChange={navigationData.setNavigation}
+          loading={loading}
+          setLoading={setLoading}
+        />
+      )}
+      {navigationData.navigation === "list" && (
+        <DataTable
+          handleUnlockCertificate={handleUnlockCertificate}
+          handleDelete={handleDelete}
+        />
+      )}
+      {navigationData.navigation === "analytics" && <Analytics />}
+      {navigationData.navigation === "archive" && <Archives />}
+      {navigationData.navigation === "instructors" && <InstructorsPage />}
+      {navigationData.navigation === "uploadCerts" && <UploadCerts />}
+    </div>
+  );
 };
 
 export default Dashboard;
