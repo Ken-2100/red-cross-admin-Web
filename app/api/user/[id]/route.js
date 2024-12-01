e199eecef68b2e5b10de6d41aa04dd6cc64366aa
ryanjoel24 <ryanjoelimba@gmail.com>
2024-12-02 05:32:22 +0800

    added users again in the dependencies in the provider

--- app/api/user/[id]/route.js
+++ app/api/user/[id]/route.js
@@ -2,56 +2,56 @@ import { NextResponse } from "next/server";
 import prisma from "@/libs/prismaDB";
 
 export async function DELETE(req, { params }) {
-	if (!params?.id) {
-		return NextResponse.json({ message: "Id is missing" }, { status: 400 });
-	}
+  if (!params?.id) {
+    return NextResponse.json({ message: "Id is missing" }, { status: 400 });
+  }
 
-	const deletedUser = await prisma.userInfo.delete({
-		where: {
-			id: params.id,
-		},
-	});
+  const deletedUser = await prisma.userInfo.delete({
+    where: {
+      id: params.id,
+    },
+  });
 
-	// console.log(`deleted user ${deletedUser.name}`); ken
+  // console.log(`deleted user ${deletedUser.name}`); ken
 
-	return NextResponse.json({ message: "user deleted" });
+  return NextResponse.json({ message: "user deleted" });
 }
 
 export async function GET(req, { params }) {
-	if (!params?.id) {
-		return NextResponse.json({ message: "Id is missing" }, { status: 400 });
-	}
-	const getUser = await prisma.userInfo.findFirst({
-		where: {
-			id: params.id,
-		},
-	});
+  if (!params?.id) {
+    return NextResponse.json({ message: "Id is missing" }, { status: 400 });
+  }
+  const getUser = await prisma.userInfo.findFirst({
+    where: {
+      id: params.id,
+    },
+  });
 
-	// console.log(`get user ${getUser.name}`); ken
+  // console.log(`get user ${getUser.name}`); ken
 
-	return NextResponse.json(getUser);
+  return NextResponse.json(getUser);
 }
 
 export async function PATCH(req, { params }) {
-	if (!params?.id) {
-		return NextResponse.json({ message: "Id is missing" }, { status: 400 });
-	}
-	const { name, email, address, contactNumber, category, gender } =
-		await req.json();
+  if (!params?.id) {
+    return NextResponse.json({ message: "Id is missing" }, { status: 400 });
+  }
+  const { name, email, address, contactNumber, category, gender } =
+    await req.json();
 
-	const updateUser = await prisma.userInfo.update({
-		where: {
-			id: params.id,
-		},
-		data: {
-			name,
-			email,
-			address,
-			contact: contactNumber,
-			category,
-			gender,
-		},
-	});
+  const updateUser = await prisma.userInfo.update({
+    where: {
+      id: params.id,
+    },
+    data: {
+      name,
+      email,
+      address,
+      contact: contactNumber,
+      category,
+      gender,
+    },
+  });
 
-	return NextResponse.json({ message: "update success" });
+  return NextResponse.json({ message: "update success" });
 }
