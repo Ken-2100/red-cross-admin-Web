9af2fc994e0a150a65adcb6703acd4f234bc2c8b
ryanjoel24 <ryanjoelimba@gmail.com>
2024-12-01 21:44:42 +0800

    deployement test 2

--- next.config.mjs
+++ next.config.mjs
@@ -12,31 +12,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*", // Apply CORS headers to all API routes
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.NEXT_PUBLIC_API_BASE_URL, // Frontend URL
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS", // Supported HTTP methods
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization", // Supported headers
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true", // Required for cookie-based authentication
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
