5410d497ee959b23b81bd0cca24b216b85da1db5
ryanjoel24 <ryanjoelimba@gmail.com>
2024-12-01 21:36:06 +0800

    updated nextjs config

--- next.config.js
+++ next.config.js
@@ -1,41 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone", // Enables standalone mode for optimized deployments
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "utfs.io",
-        port: "", // No port needed
-        pathname: "/f/**", // Matches paths under /f/
+        port: "",
+        pathname: "/f/**",
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*", // Apply CORS headers to all API routes
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.NEXT_PUBLIC_API_BASE_URL, // Frontend URL
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS", // Supported HTTP methods
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization", // Supported headers
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true", // Required for cookie-based authentication
-          },
-        ],
-      },
-    ];
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
